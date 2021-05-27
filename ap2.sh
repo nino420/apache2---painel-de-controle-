4fe541d136885237a41e0904b0d1db4d1027e54b
nino420 <68714492+nino420@users.noreply.github.com>
2021-05-27 10:14:53 -0300

    Update ap2.sh

--- ap2.sh
+++ ap2.sh
@@ -3,24 +3,28 @@
 clear
 
 echo "
-─█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ░█─░█ ░█▀▀▀ █▀█ 
-░█▄▄█ ░█▄▄█ ░█▄▄█ ░█─── ░█▀▀█ ░█▀▀▀ ─▄▀ 
-░█─░█ ░█─── ░█─░█ ░█▄▄█ ░█─░█ ░█▄▄▄ █▄▄" 
+─█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ░█─░█ ░█▀▀▀ █▀█
+░█▄▄█ ░█▄▄█ ░█▄▄█ ░█─── ░█▀▀█ ░█▀▀▀ ─▄▀
+░█─░█ ░█─── ░█─░█ ░█▄▄█ ░█─░█ ░█▄▄▄ █▄▄"
 
-echo "------------------------------------------------------------------"
+echo "----------------------------------------------------------->
 
 
-echo "  [1] kill process"
-echo "  [2] start server"
+echo "  [1]  kill process"
+echo "  [2]  start server"
 echo "  [3]  stop server"
-echo "------------------------------------------------------------------"
-echo "[digite uma opção de 1 a 3]"
+echo "  [4]  kill proocess  and start server"
+echo "  [5]  delete httpd"
+echo "----------------------------------------------------------->
+echo "[selcione de 1 a até o número 5]"
 
-read n  
+read n
 case $n in
-  1)   killall httpd;;
-  2)  apachectl start;;
-  3)  apachectl  stop;;
+  1)    killall httpd;;
+  2)    apachectl start;;
+  3)    apchectl  stop;;
+  4)    killall httpd && apachectl start;;
+  5)    rm $PREFIX/var/run/apache2/httpd.pid;;
   *)  echo "invalid option";;
-        
+
 esac
