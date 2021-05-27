b7cd71e2c1a3dd50f8d29ceb32bc68190e1f4e60
nino420 <68714492+nino420@users.noreply.github.com>
2021-05-27 07:52:44 -0300

    Add files via upload

---
+++ ap2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+
+echo "
+─█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ░█─░█ ░█▀▀▀ █▀█ 
+░█▄▄█ ░█▄▄█ ░█▄▄█ ░█─── ░█▀▀█ ░█▀▀▀ ─▄▀ 
+░█─░█ ░█─── ░█─░█ ░█▄▄█ ░█─░█ ░█▄▄▄ █▄▄" 
+
+echo "------------------------------------------------------------------"
+
+
+echo "  [1] kill process"
+echo "  [2] start server"
+echo "  [3]  stop server"
+echo "------------------------------------------------------------------"
+echo "[digite uma opção de 1 a 3]"
+
+read n  
+case $n in
+  1)   killall httpd;;
+  2)  apachectl start;;
+  3)  apchectl  stop;;
+  *)  echo "invalid option";;
+        
+esac
